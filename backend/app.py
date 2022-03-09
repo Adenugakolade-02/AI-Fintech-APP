d87e007d7099036d3fff46ff0712a23f440ed6e1
Kolade <kolade2.adenuga@gmail.com>
2022-03-09 15:28:24 +0100

    created the prediction api

--- backend/app.py
+++ backend/app.py
@@ -6,23 +6,42 @@ from typing import List
 from pydantic import BaseModel, conlist
 
 app = FastAPI(debug=True)
+standard_scale = pickle.load(open('backend/scale.pickle', 'rb'))
+model = pickle.load(open('backend/Boost.pickle','rb'))
 
 class ScaleData(BaseModel):
     data : List[conlist(int, min_items=11, max_items=11)]
 
-standard_scale = pickle.load(open('backend/scale.pickle', 'rb'))
+class TrainData(BaseModel):
+    data: List[conlist(float, min_items=28,max_items=28)]
+
 
 @app.get('/')
 def home():
     return {'text':'This is a trial'} 
 
+
 @app.post('/scale')
 def scale_data(s_data: ScaleData):
     data = dict(s_data)['data']
-    
-
     result  = standard_scale.transform(np.array(data).reshape(1,-1))
     return {'result':result.tolist()[0]}
+
+
+@app.post('/predict')
+def predict(trainData: TrainData):
+    data = dict(trainData)['data']
+    result = model.predict(np.array(data).reshape(1,-1))
+    # return {'prediction result': result.tolist()[0]}
+    if (result.tolist()[0]==0):
+        
+        return {'prediction result': 'Bad'}
+    
+    else:
+        return{'prediction result': 'Good'}
+    
+
+
 
 if __name__ == '__main__':
     uvicorn.run(app)
