683aa72006e6592cc11e1e7308946b23a2cf9c54
Kolade <kolade2.adenuga@gmail.com>
2022-03-09 11:47:59 +0100

    updated the standard scaling api

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import uvicorn
 import pickle
-from pydantic import BaseModel
+import numpy as np
+from typing import List
+from pydantic import BaseModel, conlist
 
+app = FastAPI(debug=True)
+
+class ScaleData(BaseModel):
+    data : List[conlist(int, min_items=11, max_items=11)]
+
+standard_scale = pickle.load(open('backend/scale.pickle', 'rb'))
+
+@app.get('/')
+def home():
+    return {'text':'This is a trial'} 
+
+@app.post('/scale')
+def scale_data(s_data: ScaleData):
+    data = dict(s_data)['data']
+    
+    result  = standard_scale.transform(np.array(data).reshape(1,-1))
+    
+    return {'result':result.tolist()}
+@app.post('/predict')
+
+if __name__ == '__main__':
+    uvicorn.run(app)
